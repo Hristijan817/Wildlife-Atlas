195e2db22eae0633642fcb104a3a36567fce5144
Hristijan <jankulovski.hristijan@uklo.edu.mk>
2025-08-07 20:32:22 +0200

    petti del

--- frontend/Wildlife-Atlas/src/App.jsx
+++ frontend/Wildlife-Atlas/src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import LandAnimals from "./pages/LandAnimals";
+import AirAnimals from "./pages/AirAnimals";
+import WaterAnimals from "./pages/WaterAnimals";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/kopno" element={<LandAnimals />} />
+        <Route path="/vozduh" element={<AirAnimals />} />
+        <Route path="/voda" element={<WaterAnimals />} />
       </Routes>
     </Router>
   )
